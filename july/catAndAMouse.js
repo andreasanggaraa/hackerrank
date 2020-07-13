7e285d1db98bb957556f59ad3e99940fc1f41179
andreasanggaraa <andreasanggaraa@gmail.com>
2020-07-13 16:58:41 +0700

    cat and a mouse done

---
+++ july/catAndAMouse.js
@@ -0,0 +1,12 @@
+function catAndMouse(x, y, z) {
+    const catADistance = Math.abs(z - x)
+    const catBDistance = Math.abs(z - y)
+
+    if(catBDistance > catADistance) return 'Cat A';
+    if(catADistance > catBDistance) return 'Cat B';
+    if(catADistance === catBDistance) return 'Mouse C';
+
+}
+
+console.log(catAndMouse(1, 2, 3)); // Cat B
+console.log(catAndMouse(1, 3, 2)); // Mouse C
