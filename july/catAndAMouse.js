fcfa688365a24f906ad8565387bf05ee0b1fd7c0
andreasanggaraa <andreasanggaraa@gmail.com>
2020-07-13 18:09:12 +0700

    the hurdle race done

--- july/catAndAMouse.js
+++ july/catAndAMouse.js
@@ -5,7 +5,6 @@ function catAndMouse(x, y, z) {
     if(catBDistance > catADistance) return 'Cat A';
     if(catADistance > catBDistance) return 'Cat B';
     if(catADistance === catBDistance) return 'Mouse C';
-
 }
 
 console.log(catAndMouse(1, 2, 3)); // Cat B
